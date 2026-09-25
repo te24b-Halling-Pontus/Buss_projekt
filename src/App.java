653a937609c3541589e38f1fdb823c6fc24bd885
pontus <pontushalling08@gmail.com>
2026-09-25 11:36:52 +0200

    gjort om OMsätning() så den tar hänsynt till baran samt gjort avboka() Gjort också så den visar fönster platser.

--- src/App.java
+++ src/App.java
@@ -6,14 +6,18 @@ static void main() {
 static void Meny(long[] platser) {
     boolean isRunning = true;
     while (isRunning) {
-        IO.println("1. Köp biljett\n2. lediga platser\n3. omsätning\n4. avsluta");
+        IO.println("\n1. köp biljett\n2. lediga platser\n3. omsätning\n4. avboka \n5. avsluta");
         int Val = StringToInt("Val: ");
         switch (Val) { // jag vet att man kan göra switches med strings men det ser fult ut
             case 1 -> Bokning(platser);
-            case 2 -> Platser(platser);
-            case 3 -> Omsätning();
-            case 4 -> isRunning = false;
-            default -> IO.println("Du har bara fyra val är du blind eller?");
+            case 2 -> {
+                LedigPlatser(platser);
+                IO.readln("tryck enter för att fortsätta");
+            }
+            case 3 -> Omsätning(platser);
+            case 4 -> Avboka(platser);
+            case 5 -> isRunning = false;
+            default -> IO.println("Du har bara fem val är du blind eller?");
         }
     }
 }
@@ -24,71 +28,93 @@ static void Bokning(long[] platser) {
         int val;
         while (true) {
             val = StringToInt("vilken plats vill du boka: ") - 1;
-            if (val < 0 || val > 19){IO.println("Du kan bara välja mellan 1 och 20");}
-            else{
-                if (platser[val] == 0){
-                    while (true)
-                    {
+            if (val < 0 || val > 19) {
+                IO.println("Du kan bara välja mellan 1 och 20");
+            } else {
+                if (platser[val] == 0) {
+                    while (true) {
                         long PersonnummerCheck = StringToLong("Personnummer (ÅÅÅÅMMDDXXXX): ");
-                        if (PersonnummerCheck >= 100000000000L && PersonnummerCheck <= 999999999999L)
-                        {
+                        if (PersonnummerCheck >= 100000000000L && PersonnummerCheck <= 999999999999L) {
                             platser[val] = PersonnummerCheck;
                             IO.println("plats " + (val + 1L) + " är bokad till " + platser[val]);
                             IO.readln("Tryck enter för att fortsätta");
                             break;
-                        }
-                        else{
+                        } else {
                             IO.println("Du måste använda tolvsifrikt personnummer (ÅÅÅÅMMDDXXXX)");
                         }
                     }
                     break;
-                }
-                else{
+                } else {
                     IO.println("Platsen redan tagen du får välja en annan plats");
                     IO.println("De här platserna är lediga: ");
                     LedigPlatser(platser);
                 }
             }
         }
-    }
-    else{
+    } else {
         IO.println("Det finns inga platser");
     }
 }
 
-static void Platser(long[] platser) {
-    
-}
-static void PersonnummerTillPlats(long[] platser){
-    long personnummer = StringToLong("Vad är ditt personnummer: ")
+static void PersonnummerTillPlats(long[] platser) {
+    long personnummer = StringToLong("Vad är ditt personnummer: ");
     for (int index = 0; index < platser.length; index++) {
-        if (platser[index] == personnummer)
-        {
+        if (platser[index] == personnummer) {
             IO.println("Ja du har plats " + index + 1);
-        }
-        else 
-        {
+        } else {
             IO.println("tyvärr Personnumret " + personnummer + " har ingen registrerad plats");
         }
     }
     IO.readln("tryck enter för att fortsäta");
 }
-static boolean LedigPlatser(long[] platser){
+
+static boolean LedigPlatser(long[] platser) {
     boolean ledigPlats = false;
     int platsNummer = 0;
     for (long plats : platser) {
         platsNummer++;
         if (plats == 0) {
             ledigPlats = true;
-            IO.print(platsNummer + ", ");
+            IO.print(platsNummer);
+            FönsterPlatser(platsNummer);
+            IO.print(", ");
         }
     }
     IO.println("är lediga");
     return ledigPlats;
 }
-static void Omsätning() {
-    for (int iterable_element : iterable) {
-        
+static void FönsterPlatser(long plats){
+    if (0 == (int)(plats % 4) || 1 == (int)(plats % 4)){
+        IO.print(" (förnster plats)");
+    }
+}
+static void Omsätning(long[] platser) {
+    int tjänadePengar = 0;
+    for (long plats : platser) {
+        if (plats <= 200809280000L || plats != 0) {
+            tjänadePengar += 300;
+        }
+        else if (plats > 200809280000L){
+            tjänadePengar += 150;
+        }
+
+    }
+    IO.readln("Du har tjänat " + tjänadePengar + "kr.\ntryck enter för att fortsätta");
+}
+
+static void Avboka(long[] platser) {
+    boolean platsHittad = false;
+    long personnummer = StringToLong("Vilket personnummer: ");
+    for (int index = 0; index < platser.length; index++) {
+        if (personnummer == platser[index]) {
+            platsHittad = true;
+            platser[index] = 0L;
+            IO.readln("Plats " + index + 1L + " är nu inte längre bookad till " + personnummer
+                    + "\nTryck enter för att fortsätta");
+        }
+    }
+    if (!platsHittad) {
+        IO.readln("Det finns ingen plats kopplad till" + personnummer + "\nTryck enter för att forsätta");
     }
 }
 
@@ -105,8 +131,8 @@ static int StringToInt(String s_number) {
     }
     return (number);
 }
-static Long StringToLong(String s_number)
-{
+
+static Long StringToLong(String s_number) {
     boolean temp = true;
     long number = 1L;
     while (temp) {
